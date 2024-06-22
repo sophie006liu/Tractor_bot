626164fc00ca3bc5d7486f93e9894e879ef34fe1
Terrance <terranceli.terbear@gmail.com>
2024-06-22 22:34:54 +0000

    Mount ssh creds and gitconfig

--- docker/start.sh
+++ docker/start.sh
@@ -8,7 +8,11 @@ docker rm unified_docker
 docker build -f $DIR/Dockerfile $DIR/.. --tag unified_docker
 
 # Start the container and mount the project repo to /workspace.
-docker run --name unified_docker -v $DIR/..:/workspace -itd unified_docker bash
+docker run -itd --name unified_docker \
+  -v $DIR/..:/workspace \
+  -v ~/.ssh:/root/.ssh \
+  -v ~/.gitconfig:/root/.gitconfig \
+  unified_docker bash
 
 # Add a step to source the provide
 docker exec -it unified_docker bash -c "echo '[[ -f /workspace/docker/.bashrc ]] && source /workspace/docker/.bashrc' >> ~/.bashrc"
